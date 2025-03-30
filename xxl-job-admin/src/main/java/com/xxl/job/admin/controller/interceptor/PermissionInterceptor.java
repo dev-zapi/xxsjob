5cd37271fe966b7a537bdf1a861f9eff9bcdec4d
Jonas Gao <gaopinsong@live.cn>
2025-03-30 16:04:24 +0800

    perf(admin): optimize permission interceptor for better error handling
    - Add logging for incoming requests
    - Skip error controller to avoid double handling of errors
    - Improve code readability and formatting

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/interceptor/PermissionInterceptor.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/interceptor/PermissionInterceptor.java
@@ -4,6 +4,8 @@ import com.xxl.job.admin.controller.annotation.PermissionLimit;
 import com.xxl.job.admin.core.model.XxlJobUser;
 import com.xxl.job.admin.core.util.I18nUtil;
 import com.xxl.job.admin.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
@@ -18,42 +20,51 @@ import javax.servlet.http.HttpServletResponse;
  * @author xuxueli 2015-12-12 18:09:04
  */
 @Component
+@Slf4j
 public class PermissionInterceptor implements AsyncHandlerInterceptor {
 
-	@Resource
-	private LoginService loginService;
+    @Resource
+    private LoginService loginService;
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		
-		if (!(handler instanceof HandlerMethod)) {
-			return true;	// proceed with the next interceptor
-		}
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-		// if need login
-		boolean needLogin = true;
-		boolean needAdminuser = false;
-		HandlerMethod method = (HandlerMethod)handler;
-		PermissionLimit permission = method.getMethodAnnotation(PermissionLimit.class);
-		if (permission!=null) {
-			needLogin = permission.limit();
-			needAdminuser = permission.adminuser();
-		}
+        if (!(handler instanceof HandlerMethod)) {
+            return true;    // proceed with the next interceptor
+        }
 
-		if (needLogin) {
-			XxlJobUser loginUser = loginService.ifLogin(request, response);
-			if (loginUser == null) {
-				response.setStatus(302);
-				response.setHeader("location", request.getContextPath()+"/toLogin");
-				return false;
-			}
-			if (needAdminuser && loginUser.getRole()!=1) {
-				throw new RuntimeException(I18nUtil.getString("system_permission_limit"));
-			}
-			request.setAttribute(LoginService.LOGIN_IDENTITY_KEY, loginUser);
-		}
+        log.info("Handling uri={}, handler={}", request.getRequestURI(), handler);
 
-		return true;	// proceed with the next interceptor
-	}
-	
+        // Skip ErrorController
+        Object bean = ((HandlerMethod) handler).getBean();
+        if (bean instanceof BasicErrorController) {
+            return true;
+        }
+
+        // if need login
+        boolean needLogin = true;
+        boolean needAdminuser = false;
+        HandlerMethod method = (HandlerMethod) handler;
+        PermissionLimit permission = method.getMethodAnnotation(PermissionLimit.class);
+        if (permission != null) {
+            needLogin = permission.limit();
+            needAdminuser = permission.adminuser();
+        }
+
+        if (needLogin) {
+            XxlJobUser loginUser = loginService.ifLogin(request, response);
+            if (loginUser == null) {
+                response.setStatus(302);
+                response.setHeader("location", request.getContextPath() + "/toLogin");
+                return false;
+            }
+            if (needAdminuser && loginUser.getRole() != 1) {
+                throw new RuntimeException(I18nUtil.getString("system_permission_limit"));
+            }
+            request.setAttribute(LoginService.LOGIN_IDENTITY_KEY, loginUser);
+        }
+
+        return true;    // proceed with the next interceptor
+    }
+
 }
